e33d056b6cdac77c2ad912b3e391cb68b42423f8
Hao Jiang <Hao Jiang>
2017-02-21 23:48:55 -0500

    change server folder. except views.

--- server/app.js
+++ server/app.js
@@ -16,7 +16,31 @@ let logger = require('morgan');
 let cookieParser = require('cookie-parser');
 let bodyParser = require('body-parser');
 
+
+//modules for authentication
+let session = require('express-session');
+let passport = require('passport');
+let passportlocal = require('passport-local');
+let LocalStrategy = passportlocal.Strategy;
+let flash = require('connect-flash'); // displays errors / login messages
+
+
+// import "mongoose" - required for DB Access
+let mongoose = require('mongoose');
+// URI
+let config = require('./config/db');
+
+mongoose.connect(process.env.URI || config.URI);
+
+let db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+  console.log("Conneced to MongoDB...");
+});
+
+// define routers
 let index = require('./routes/index');
+let userinfos = require('./routes/userinfos'); 
 
 
 let app = express();
@@ -30,27 +54,48 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '../client')));
 
+//app.use('/', index);
+// setup session
+app.use(session({
+  secret: "SomeSecret",
+  saveUninitialized: true,
+  resave: true
+}));
+
+// initialize passport and flash
+app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
+
+// route redirects
 app.use('/', index);
+app.use('/userinfos', userinfos);
+
+// Passport User Configuration
+let UserModel = require('./models/users');
+let User = UserModel.User; // alias for the User Model - User object
+passport.use(User.createStrategy());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 
-// catch 404 and forward to error handler
-app.use((req, res, next)=> {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+// Handle 404 Errors
+  app.use(function(req, res) {
+      res.status(400);
+     res.render('errors/404',{
+      title: '404: File Not Found'
+    });
+  });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+  // Handle 500 Errors
+  app.use(function(error, req, res, next) {
+      res.status(500);
+      res.render('errors/500', {
+        title:'500: Internal Server Error',
+        error: error
+      });
+  });
 
 module.exports = app;
