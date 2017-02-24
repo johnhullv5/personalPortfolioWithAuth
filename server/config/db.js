ebd8c844fe3cd2eb5e1e7bd48a327e4ba1edb8f1
Hao Jiang <Hao Jiang>
2017-02-24 13:54:29 -0500

    delete some comments.

--- server/config/db.js
+++ server/config/db.js
@@ -2,5 +2,5 @@ module.exports = {
   //local MongoDB deployment ->
   //userinfo is the database name
   "URI": "mongodb://localhost/userinfo"
-  //Remote MongoDB deployment -> let URI = "mongodb://thomas:123456@ds054999.mlab.com:54999/videogames";
+ 
 };
